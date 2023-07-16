3746a3f22df267b427e66553286c48de939f4489
Thái Nguyễn <thaivannguyen.it@gmail.com>
2023-07-16 23:38:29 +0700

    Add service api

---
+++ src/lib/index.ts
@@ -0,0 +1,174 @@
+import qs from 'qs';
+
+enum HttpStatusCode {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+}
+
+type ResponseData = Record<string, any>;
+type ResponseType = 'json' | 'text' | 'blob'; // 'arraybuffer' | 'document' | 'stream' --> those type will use when need
+
+interface RequestProperty {
+  url: string;
+  method: 'CONNECT' | 'DELETE' | 'GET' | 'HEAD' | 'OPTIONS' | 'POST' | 'PUT';
+  params?: Record<string, any>;
+  data?: any;
+  headers?: Record<string, any>;
+  transformRequest?: (data: any) => any;
+  responseType?: ResponseType;
+}
+
+interface RequestOption extends RequestProperty {
+  isAuth?: boolean;
+}
+
+const RequestPropertyInit: RequestProperty = {
+  url: '',
+  method: 'GET',
+  transformRequest: (data) => data,
+  responseType: 'json',
+};
+
+const convertResponse = async (response: Response, responseType: ResponseType): Promise<any> => {
+  switch (responseType) {
+    case 'json':
+      return await response.json();
+    case 'text':
+      return await response.text();
+    case 'blob':
+      return await response.blob();
+    default:
+      return response;
+  }
+};
+
+let isRefreshToken: boolean = false;
+let refreshSubscribers: Record<string, any>[] = [];
+
+// Function to add a request subscriber to the pending queue
+const addSubscriber = (subscriber: Record<string, any>) => {
+  refreshSubscribers.push(subscriber);
+};
+
+const refreshToken = async (refreshToken?: string) => {
+  try {
+    const response = await fetch('http://localhost:8080/api/auth/refreshtoken', {
+      method: 'POST',
+      body: JSON.stringify({ refreshToken: refreshToken }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      window.localStorage.setItem('access_token', data.accessToken);
+      window.localStorage.setItem('refresh_token', data.refreshToken);
+      refreshSubscribers.forEach((subscriber) => subscriber.resolve(data.accessToken));
+      refreshSubscribers = [];
+    } else if (response.status === 403) {
+      alert('Please login again !');
+      // window.location.replace('/');
+      throw new Error('Refresh token is expired');
+    } else {
+      throw new Error('Failed to refresh token');
+    }
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'Failed to refresh token');
+  } finally {
+    isRefreshToken = false;
+  }
+};
+
+const makeRequest = async (props = RequestPropertyInit): Promise<ResponseData> => {
+  const { url, method, params, data, headers, transformRequest } = props;
+  const responseType = props.responseType || 'json';
+  let resource = url;
+  const requestHeaders: Record<string, any> = { ...headers };
+  const requestOption: RequestInit = {
+    method,
+    headers: requestHeaders,
+  };
+  if (method === 'GET' && params) {
+    resource = `${resource}${qs.stringify(params)}`;
+  } else {
+    requestOption.body = transformRequest?.(data) || data;
+  }
+
+  try {
+    const response = await fetch(resource, requestOption);
+    if (response.ok) {
+      const responseData = await convertResponse(response, responseType);
+      return {
+        success: true,
+        responseData,
+      };
+    } else if (response.status == HttpStatusCode.UNAUTHORIZED) {
+      if (!isRefreshToken) {
+        isRefreshToken = true;
+        await refreshToken(window.localStorage.getItem('refresh_token') || '');
+      }
+      const retryRequest = new Promise((resolve, reject) => {
+        addSubscriber({ resolve, reject });
+      });
+      await retryRequest;
+      return await makeRequest({
+        ...props,
+        headers: {
+          ...props.headers,
+          'x-access-token': window.localStorage.getItem('access_token'),
+        },
+      });
+    } else if (response.status === HttpStatusCode.FORBIDDEN) {
+      throw new Error('Forbidden');
+    } else {
+      const errorData = await convertResponse(response, responseType);
+      throw new Error(errorData.message || 'Request failed');
+    }
+  } catch (error) {
+    return {
+      success: false,
+      errorMessage: error instanceof Error ? error.message : '',
+    };
+  }
+};
+
+export const doRequest = async (requestOption: RequestOption) => {
+  const { isAuth, method, headers, params, data, ...requestProperty } = requestOption;
+  let headersCustom: Record<string, any> = {
+    ...headers,
+    ...(isAuth && {
+      Authorization: `Bearer ${window.localStorage.getItem('access_token')}`,
+      'x-access-token': `${window.localStorage.getItem('access_token')}`,
+    }),
+  };
+
+  switch (method) {
+    case 'GET':
+      return await makeRequest({
+        ...requestProperty,
+        method: method,
+        headers: headersCustom,
+        params: params,
+      });
+    default:
+      let transformRequest = (dataRequest: any) => dataRequest;
+
+      switch (headers?.['Content-Type']) {
+        case 'application/json':
+          transformRequest = (dataRequest: string) => JSON.stringify(dataRequest);
+          break;
+        default:
+          break;
+      }
+      return await makeRequest({
+        ...requestProperty,
+        method: method,
+        headers: headersCustom,
+        data: data,
+        transformRequest: transformRequest,
+      });
+  }
+};
---
+++ src/lib/test.ts
@@ -0,0 +1,79 @@
+import { doRequest } from './index';
+
+type ResponseData = Record<string, any>;
+
+export const getAll = async (): Promise<ResponseData> => {
+  const response = await doRequest({
+    url: 'http://localhost:8080/api/test/all',
+    method: 'GET',
+    responseType: 'text',
+  });
+  console.log('getAll' + new Date().getTime(), response);
+  return response;
+};
+
+export const getUser = async (): Promise<ResponseData> => {
+  const response = await doRequest({
+    url: 'http://localhost:8080/api/test/user',
+    method: 'GET',
+    responseType: 'text',
+    isAuth: true,
+  });
+  console.log('getUser' + new Date().getTime(), response);
+  return response;
+};
+
+export const getAdmin = async (): Promise<ResponseData> => {
+  const response = await doRequest({
+    url: 'http://localhost:8080/api/test/admin',
+    method: 'GET',
+    responseType: 'text',
+    isAuth: true,
+  });
+  console.log('getAdmin', response);
+  return response;
+};
+
+export const signUp = async (): Promise<ResponseData> => {
+  const response = await doRequest({
+    // url: 'https://api-gateway.dev.pharmacity.io/open_stores',
+    // url: 'https://api-gateway.dev.pharmacity.io/customers/auth/me',
+    // url: 'https://api.nhathuoclongchau.com.vn/lccus/ecom-prod/customer-api/v2/token/order?maxResultCount=5&skipCount=0&tabCode=0',
+    // url: 'https://api-gateway.dev.pharmacity.io/erp/extracare-info',
+    url: 'http://localhost:8080/api/auth/signup',
+    method: 'POST',
+    data: {
+      // client_id: 'cpxKo0Qu0DBQbrtwOPOfmgHxtOnLUP1W',
+      // client_secret: 'P6nYG4brmOKDSij6UL4kAZKwNs9ONwq1pvLcmIVY-XcHPWa7otYMpuMkccGALhNr',
+      // audience: 'https://dev-a2glpje16emmmsat.us.auth0.com/api/v2/',
+      // grant_type: 'client_credentials',
+      email: 'thaivannguyen.it@gmail.com',
+      password: '12345678',
+      username: 'thai',
+      role: 'admin',
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return response;
+};
+
+export const signIn = async (): Promise<ResponseData> => {
+  const response = await doRequest({
+    url: 'http://localhost:8080/api/auth/signin',
+    method: 'POST',
+    data: {
+      username: 'thai',
+      password: '12345678',
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  console.log('response', response);
+  window.localStorage.setItem('access_token', response.responseData.accessToken);
+  window.localStorage.setItem('refresh_token', response.responseData.refreshToken);
+  return response;
+};
