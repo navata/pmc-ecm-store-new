b7062a2b4e33f1af1c56119966a58c0a8a4baffd
PMCW1127\Thai Digital <thai.nv@pharmacity.vn>
2023-07-30 23:08:09 +0700

    Test commit from window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         onClick={signIn}
         className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
       >
-        signIn
+        signInWindow
       </button>
       <button
         onClick={getMultiUser}
